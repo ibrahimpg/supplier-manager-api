ba8f58ff7dfd89f729cf09b452922bac82d19d85
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2021-09-18 02:48:26 +0300

    cors

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const api = require('./api/routes');
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+
 app.use('/api', api);
 
 app.listen(process.env.PORT);
